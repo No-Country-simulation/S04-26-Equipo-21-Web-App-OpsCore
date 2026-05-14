e34cd624e25c99e86caebdf3eaa84cce9f848fee
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-05-14 15:57:52 -0600

    Se agrego la implementación de user details para spring security

--- Backend/incident-management/src/main/java/com/opscore/incident/model/Usuario.java
+++ Backend/incident-management/src/main/java/com/opscore/incident/model/Usuario.java
@@ -6,17 +6,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "usuarios")
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
-public class Usuario extends BaseEntity {
+public class Usuario extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+
+    private String username;
 
     @Column(unique = true)
     private String numeroReloj;
@@ -43,4 +50,8 @@ public class Usuario extends BaseEntity {
     private Set<Especialidad> especialidades;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.rol.name()));
+    }
 }
