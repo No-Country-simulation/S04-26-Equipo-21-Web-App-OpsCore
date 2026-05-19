2d33b4f0045a634631ee18d826ea5d3e645895f2
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-05-18 19:50:36 -0600

    fix(cookie): add Env SECURE_COOKIE for https

--- Backend/incident-management/src/main/java/com/opscore/incident/service/AuthCookieService.java
+++ Backend/incident-management/src/main/java/com/opscore/incident/service/AuthCookieService.java
@@ -1,15 +1,19 @@
 package com.opscore.incident.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthCookieService {
 
+    @Value( "${security.app.secure-cookie}")
+    private boolean secureCookie;
+
     public ResponseCookie accessTokenCookie(String token, long minutes) {
         return ResponseCookie.from("access_token", token)
                 .httpOnly(true)
-                .secure(true)
+                .secure(secureCookie)
                 .path("/")
                 .maxAge(minutes * 60)
                 .sameSite("Lax")
@@ -19,7 +23,7 @@ public class AuthCookieService {
     public ResponseCookie refreshTokenCookie(String token, long hours) {
         return ResponseCookie.from("refresh_token", token)
                 .httpOnly(true)
-                .secure(true)
+                .secure(secureCookie)
                 .path("/api/auth/refresh")
                 .maxAge(hours * 3600)
                 .sameSite("Lax")
